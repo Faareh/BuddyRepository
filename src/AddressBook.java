266466fd42c981c77f1c15858755337a47ee18ac
Faareh <faarehb@yahoo.com>
2023-09-24 17:29:37 -0400

    Lab 3 Step 11

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -27,6 +27,6 @@ public class AddressBook {
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(0);
-        System.out.println("New change");
+        System.out.println("New change to push");
     }
 }
